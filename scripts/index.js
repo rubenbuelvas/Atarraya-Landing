de4c6cc2833be539433507883ebfceb94d7d580b
José Nieto <jdnv.123@gmail.com>
2018-09-10 23:44:53 -0500

    Rubén's job made for him

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,9 @@ function sendData() {
     });
 }
 
+document.getElementById("myForm").addEventListener("keypress", function(event) {
+    if(event.which == 13) {
+        event.preventDefault();
+        sendData();
+    }
+});
